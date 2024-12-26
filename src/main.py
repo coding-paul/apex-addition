beff1e7da8eec35632bc430d623bf699044a8832
Pauel <paul@doebertin.com>
2024-12-26 20:29:01 +0100

    Update imports and moved mouse movements to main.py

--- src/main.py
+++ src/main.py
@@ -1,2 +1,31 @@
+import json
+import threading
+import time
+from pynput import keyboard
+from pynput.mouse import Controller, Button
+
+app_running = False
+
 def main():
   print("Test")
+
+mouse = Controller()
+
+def load_pattern():
+  with open('pattern.json', 'r') as file:
+      return json.load(file)
+
+def move_mouse_pattern():
+  pattern = load_pattern()
+  for move in pattern:
+      if not app_running:
+          break
+      mouse.move(move['x'], move['y'])
+      time.sleep(move['delay'])
+
+def on_click(x, y, button, pressed):
+  if app_running and button == Button.left and pressed:
+      threading.Thread(target=move_mouse_pattern).start()
+
+mouse_listener = keyboard.Listener(on_click=on_click)
+mouse_listener.start()
--- src/user_interface.py
+++ src/user_interface.py
@@ -1,9 +1,5 @@
 import tkinter as tk
 from pynput import keyboard
-import json
-import threading
-import time
-from pynput.mouse import Controller, Button
 import main # Import main app from same directory
 
 is_main_screen = False
@@ -107,24 +103,3 @@ def start_listener():
 
 start_listener()
 show_title_screen()
-
-mouse = Controller()
-
-def load_pattern():
-    with open('pattern.json', 'r') as file:
-        return json.load(file)
-
-def move_mouse_pattern():
-    pattern = load_pattern()
-    for move in pattern:
-        if not app_running:
-            break
-        mouse.move(move['x'], move['y'])
-        time.sleep(move['delay'])
-
-def on_click(x, y, button, pressed):
-    if app_running and button == Button.left and pressed:
-        threading.Thread(target=move_mouse_pattern).start()
-
-mouse_listener = keyboard.Listener(on_click=on_click)
-mouse_listener.start()
