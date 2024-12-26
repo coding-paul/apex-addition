a0bad5fc4bb0e23d7b6f2e63f42052ac7114a135
Pauel <paul@doebertin.com>
2024-12-27 00:48:17 +0100

    Check tracker.py

--- src/tracker.py
+++ src/tracker.py
@@ -1,20 +1,39 @@
 import time
+import os
 from PIL import ImageGrab
-import pytesseract
 from PIL import Image
+import pytesseract
 import threading
 
+import uuid
+
 # Stelle sicher, dass der Tesseract-Pfad korrekt ist
-pytesseract.pytesseract.tesseract_cmd = r'C:\Users\Finwa\AppData\Local\Programs\Tesseract-OCR\tesseract.exe'
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\paul\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
 # Definiere den Bereich (x1, y1, x2, y2)
 bbox1 = (1550, 1030, 1675, 1060)
 bbox2 = (1715, 1030, 1815, 1060)
 
-current_weapon: str = ""
+DELAY = 5 # Zeitintervall, um Ressourcen zu schonen in Sekunden
+
+current_weapon: str = None
 
 weapon1_text = ""
 weapon2_text = ""
+
+def get_absolute_path(path: str) -> str:
+  script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
+  abs_file_path = os.path.join(script_dir, path)
+  return abs_file_path
+
+def update_weapon(new_weapon: str):
+    global current_weapon
+    with weapon_lock:
+        current_weapon = new_weapon
+
+def get_current_weapon() -> str:
+    with weapon_lock:
+        return current_weapon
 
 def live_text_tracking():
     global weapon1_text, weapon2_text
@@ -24,12 +43,14 @@ def live_text_tracking():
             screenshot1 = ImageGrab.grab(bbox1)
             screenshot2 = ImageGrab.grab(bbox2)
 
+            screenshot1.save(get_absolute_path("./screenshots/screenshot1_" + str(uuid.uuid4()) + ".png"))
+
             # Text aus dem Screenshot extrahieren
             weapon1_text = pytesseract.image_to_string(screenshot1).strip()
             weapon2_text = pytesseract.image_to_string(screenshot2).strip()
             
             # Zeitintervall, um Ressourcen zu schonen
-            time.sleep(0.5)
+            time.sleep(DELAY)
     except KeyboardInterrupt:
         print("Live-Tracking beendet.")
 
@@ -59,9 +80,9 @@ def get_color_at_position(x, y):
     return color
 
 def color_checking():
-    global current_weapon
     try:
         while True:
+            sucessfull = False
             for (x, y), target_color in coordinates_and_colors_weapon1:
                 # Get the color at the specified position
                 color = get_color_at_position(x, y)
@@ -69,7 +90,8 @@ def color_checking():
                 # Check if the color matches the target color
                 if color == target_color:
                     print(f"Weapon 1 ({weapon1_text})")
-                    current_weapon = weapon1_text
+                    update_weapon(weapon1_text)
+                    sucessfull = True
                     break
             
             for (x, y), target_color in coordinates_and_colors_weapon2:
@@ -79,22 +101,30 @@ def color_checking():
                 # Check if the color matches the target color
                 if color == target_color:
                     print(f"Weapon 2 ({weapon2_text})")
-                    current_weapon = weapon2_text
+                    update_weapon(weapon2_text)
                     break
+                elif not sucessfull:
+                    print("No weapon detected")
             
             # Time interval to save resources
-            time.sleep(0.5)
+            time.sleep(DELAY)
     except KeyboardInterrupt:
         print("Color checking stopped.")
 
-# Create threads for both functions
-thread1 = threading.Thread(target=live_text_tracking)
-thread2 = threading.Thread(target=color_checking)
+if __name__ == "__main__":
+    # Create threads for both functions
+    thread1 = threading.Thread(target=live_text_tracking)
+    thread2 = threading.Thread(target=color_checking)
 
-# Start the threads
-thread1.start()
-thread2.start()
+    # Start the threads
+    thread1.start()
+    thread2.start()
 
-# Wait for both threads to complete
-thread1.join()
-thread2.join()
+    # Create a lock for the current_weapon variable
+    weapon_lock = threading.Lock()
+
+    print("Tracker started")
+
+    # Wait for both threads to complete
+    thread1.join()
+    thread2.join()
