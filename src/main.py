add40bda2aaf6cb019e8d22b08cfb8b90bbc445b
Pauel <paul@doebertin.com>
2024-12-28 14:22:04 +0100

    Edit name of variable

--- src/main.py
+++ src/main.py
@@ -21,9 +21,9 @@ def quit():
     tracker_stop_event.set()
     return False
 
-def get_absolute_path(path: str) -> str:
+def get_absolute_path(rel_path: str) -> str:
   script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-  abs_file_path = os.path.join(script_dir, path)
+  abs_file_path = os.path.join(script_dir, rel_path)
   return abs_file_path
 
 def get_current_weapon() -> str:
