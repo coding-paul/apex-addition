44394e0d89ce992ec5d067db75ef4d809bf7cad4
Pauel <paul@doebertin.com>
2024-12-28 22:33:35 +0100

    Remove unnecessary function
    get_absolute_path was moved to utils but still present in main which was not used

--- src/main.py
+++ src/main.py
@@ -20,11 +20,6 @@ def quit():
     tracker_stop_event.set()
     return False
 
-def get_absolute_path(rel_path: str) -> str:
-  script_dir = os.path.dirname(__file__) #<-- absolute dir the script is in
-  abs_file_path = os.path.join(script_dir, rel_path)
-  return abs_file_path
-
 def load_pattern() -> tuple[list, str]:
     print("\nLoading pattern...")
     weapon_scan: str = tracker_get_current_weapon()  # Is None from beginning but is a valid name of a weapon in the recoil_patterns.json file once a weapon is detected
